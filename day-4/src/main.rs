c51663c4733409a20d775b53b12b4e0839352835
José Manuel González Ricart <jmanuelgricart@gmail.com>
2023-12-11 20:58:30 -0300

    Add day 4 part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,7 +1,8 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, collections::HashMap};
 
 fn main() {
-     println!("the sum of points {}", get_the_points("input"));
+    println!("the sum of points {}", get_the_points("input"));
+    println!("the number of cards {}", get_the_number_of_cards("input"));
 }
 
 fn get_the_points(input: &str) -> usize {
@@ -36,6 +37,52 @@ fn get_the_points(input: &str) -> usize {
     sum
 }
 
+fn get_the_number_of_cards(input: &str) -> usize {
+    let data = read_to_string(input).unwrap();
+    let mut cards_coincidences: HashMap<String, usize> = HashMap::new(); 
+    let mut cards_amount: HashMap<String, usize> = HashMap::new();
+
+    for card in data.lines().collect::<Vec<&str>>().iter() {
+        let (card_name, numbers) = card
+            .split_once(":")
+            .unwrap();
+        let (winning_numbers, my_numbers) = numbers.split_once("|").unwrap(); 
+        let winning: Vec<&str> = winning_numbers.trim().split(" ").filter(|val| val.len() > 0).collect();
+        let numbers: Vec<&str> = my_numbers.trim().split(" ").filter(|val| val.len() > 0).collect();
+        let coincidences: Vec<&str> = numbers.iter().filter(|number| winning.contains(*number)).map(|number| *number).collect();
+
+        cards_coincidences.entry(card_name.to_string()).or_insert(coincidences.len());
+        cards_amount.entry(card_name.to_string()).or_insert(1);
+    }
+        
+    for index in 1..=cards_amount.len() {
+        let key = get_key(index);
+        let conincidences = cards_coincidences.get(&key).unwrap();
+        let current_copies = cards_amount.get(&key).unwrap();
+
+        for _ in 1..=*current_copies {
+            for coinc_index in 1..=*conincidences {
+                let coinc_key = get_key(index + coinc_index);
+
+                *cards_amount.get_mut(&coinc_key).unwrap() += 1;
+            }
+        }
+    } 
+
+    cards_amount.into_iter().map(|(_, v)| v).sum()
+}
+
+fn get_key(index: usize) -> String {
+    if index < 10 {
+        return format!("Card   {}", index);
+    } else if index < 100 {
+        return format!("Card  {}", index);
+    } else {
+        return format!("Card {}", index);
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,4 +92,8 @@ mod tests {
         assert_eq!(get_the_points("input_test"), 13);
     }
 
+    #[test]
+    fn it_should_get_the_number_of_cards() {
+        assert_eq!(get_the_number_of_cards("input_test"), 30);
+    }
 }
